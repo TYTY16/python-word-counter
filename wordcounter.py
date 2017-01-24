f9d09a9008208ddc6f27f86925ea80dd8945dbf8
Tyler Tedeschi <ttedesch@ucsd.edu>
2017-01-24 01:14:45 -0800

    Create dictionary with frequency of words, but does not handle punctuation

--- wordcounter.py
+++ wordcounter.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 import sys
+import string
+
+wordDictionary = {};
 
 file = open(sys.argv.pop(1), 'r');
 
-for line in f:
-	print line
+for line in file:
+	words = line.split();
+	for word in words:
+		strippedWord = word.translate(string.punctuation);
+		if strippedWord in wordDictionary:
+			wordDictionary[strippedWord] = wordDictionary[strippedWord] + 1;
+		else:
+			wordDictionary[strippedWord] = 1;
 
+print(str(wordDictionary));
